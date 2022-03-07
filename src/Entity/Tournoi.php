3c7fd7f34cb414f08510cb77ff51957226652438
azizbenzina9 <98781148+azizbenzina9@users.noreply.github.com>
2022-03-07 19:31:13 +0100

    Add files via upload

---
+++ src/Entity/Tournoi.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TournoiRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=TournoiRepository::class)
+ * @ORM\HasLifecycleCallbacks
+ */
+class Tournoi
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    
+    private $nomTournoi;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    
+    private $jeux;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $DateDebut;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $DateFin;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Couleur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomTournoi(): ?string
+    {
+        return $this->nomTournoi;
+    }
+
+    public function setNomTournoi(string $nomTournoi): self
+    {
+        $this->nomTournoi = $nomTournoi;
+
+        return $this;
+    }
+
+    public function getJeux(): ?string
+    {
+        return $this->jeux;
+    }
+
+    public function setJeux(string $jeux): self
+    {
+        $this->jeux = $jeux;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->DateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $DateDebut): self
+    {
+        $this->DateDebut = $DateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->DateFin;
+    }
+
+    public function setDateFin(\DateTimeInterface $DateFin): self
+    {
+        $this->DateFin = $DateFin;
+
+        return $this;
+    }
+
+    public function __toString(){
+       
+        return $this->nomTournoi;
+    }
+
+    public function _toString(){
+       
+        return $this->DatedebMatch;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->Couleur;
+    }
+
+    public function setCouleur(string $Couleur): self
+    {
+        $this->Couleur = $Couleur;
+
+        return $this;
+    }
+}
